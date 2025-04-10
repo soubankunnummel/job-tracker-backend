e8bf82f82c897c08e3d9b5075ec7405751d6f80c
Muhammed souban <muhammedsoubankunnummel123@gmail.com>
2025-04-10 21:31:59 +0530

    add: cron job

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import errorHandler from "./middleware/error-handler";
 import jobRoutes from "./routes/job-route";
-
+import cron from "node-cron";
 const app = express();
 
 //middleware
@@ -13,6 +13,12 @@ app.use(cors());
 
 // routes
 app.use("/api/v1/jobs", jobRoutes);
+
+// Cron Job: Runs every second
+cron.schedule("*/15 * * * * *", () => {
+    console.log("Cron job triggered every second");
+    // Add your logic here (e.g., database cleanup, sending notifications, etc.)
+  });
 app.use(errorHandler);
 
 export default app;
